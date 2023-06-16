160c1bfc8e71a3d1c832b986cafd2cea8967d993
Priyanshu <priyanshupanditlg12a@gmail.com>
2023-06-16 22:00:28 +0530

    added new projects

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -2,31 +2,47 @@ import Littlelemon from '../assets/projects/littlelemon.png'
 import Calculator from '../assets/projects/calculator.png'
 import DigitalClock from '../assets/projects/digitalClock.png'
 import Animation from '../assets/projects/animation.jpg'
+import GymApp from '../assets/projects/gymApp.png'
+import ReactAdmin from '../assets/projects/reactAdmin.png'
 
 export const data=[
     {
         id:1,
+        name:"Gym App",
+        image:GymApp,
+        github:"https://github.com/Priyanshu255/Gym-App",
+        live:"https://priyanshu255.github.io/Gym-App/",
+    },
+    {
+        id:2,
+        name:"React Admin Dashboard",
+        image:ReactAdmin,
+        github:"https://github.com/Priyanshu255/React-Admin-Dashboard",
+        live:"https://priyanshu255.github.io/React-Admin-Dashboard/",
+    },
+    {
+        id:3,
         name:"Little Lemon Website",
         image:Littlelemon,
         github:"https://github.com/Priyanshu255/Little-Lemon-Restaurent-Website",
         live:"https://priyanshu255.github.io/Little-Lemon-Restaurent-Website/",
     },
     {
-        id:2,
+        id:4,
         name:"Calculator",
         image:Calculator,
         github:"https://github.com/Priyanshu255/Calculator",
         live:"https://priyanshu255.github.io/Calculator/",
     },
     {
-        id:3,
+        id:5,
         name:"Digital Clock",
         image:DigitalClock,
         github:"https://github.com/Priyanshu255/Digital-Clock",
         live:"https://priyanshu255.github.io/Digital-Clock/",
     },
     {
-        id:4,
+        id:6,
         name:"Animations",
         image:Animation,
         github:"https://github.com/Priyanshu255/Animations",
